817518ae788b4afb98a937ab9916f3e5f918c9d0
Carlos Souza <carlosaugsz@hotmail.com>
2024-12-11 14:50:24 -0300

    complete crud operations

--- src/todos-routes.js
+++ src/todos-routes.js
@@ -1,22 +1,71 @@
 const express = require("express")
-
-const allTodos = [{nome: "aaaa", status: false}]
 const todosRoutes = express.Router()
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient()
 
 // Create
-todosRoutes.post("/todos", (request, response) => {
+todosRoutes.post("/todos", async (request, response) => {
     const { name } = request.body
-    allTodos.push({ name, status: false })
-    return response.status(201).json(allTodos)
+    const todo = await prisma.todo.create({
+        data: {
+            name,
+        }
+    })
+    return response.status(201).json(todo)
 })
 
 // Read
-todosRoutes.get("/todos", (request, response) => {
-    return response.status(200).json(allTodos)
+todosRoutes.get("/todos", async (request, response) => {
+    const todos = await prisma.todo.findMany()
+    return response.status(200).json(todos)
 })
 
 // Update
+todosRoutes.put("/todos", async (request, response) =>{
+    const { name, id, status } = request.body
+
+    if(!id) {
+        return response.status(400).json("Id is mandatory")
+    }
+
+    const todoAlreadyExist = await prisma.todo.findUnique({ where: { id }})
+
+    if(!todoAlreadyExist) {
+        return response.status(404).json("Todo don't exist")
+    }
+
+    const todo = await prisma.todo.update({
+        where: {
+            id,
+        },
+        data: {
+            name,
+            status,
+        },
+    })
+    return response.status(200).json(todo)
+})
 
 // Delete
+todosRoutes.delete("/todos/:id", async (request, response) => {
+    const { id } = request.params
+
+    const intId = parseInt(id)
+
+    if(!intId) {
+        return response.status(400).json("Id is mandatory")
+    }
+
+    const todoAlreadyExist = await prisma.todo.findUnique({ where: { id: intId }})
+
+    if(!todoAlreadyExist) {
+        return response.status(404).json("Todo don't exist")
+    }
+
+    await prisma.todo.delete({ where: { id: intId } })
+    
+    return response.status(200).send()
+})
 
 module.exports = todosRoutes
