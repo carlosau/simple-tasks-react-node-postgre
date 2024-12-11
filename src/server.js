dbc958f3b8c5f61c1157e2872b4c8f2a6d9e5170
Carlos Souza <carlosaugsz@hotmail.com>
2024-12-11 12:58:05 -0300

    server ok and routes for create tasks and read

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require("express")
 const todosRoutes = require("./todos-routes")
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
+app.use(todosRoutes);
 
 app.get("/health", (req, res) => {
     return res.json("up")
---
+++ src/todos-routes.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const allTodos = [{nome: "aaaa", status: false}]
+const todosRoutes = express.Router()
+
+// Create
+todosRoutes.post("/todos", (request, response) => {
+    const { name } = request.body
+    allTodos.push({ name, status: false })
+    return response.status(201).json(allTodos)
+})
+
+// Read
+todosRoutes.get("/todos", (request, response) => {
+    return response.status(200).json(allTodos)
+})
+
+// Update
+
+// Delete
+
+module.exports = todosRoutes
